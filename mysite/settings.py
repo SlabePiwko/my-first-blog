46f3b39652a865cf353c3cc8a3c3ecfc1cf69b98
Twoja Nazwa Uzytkownika <ty@adres.pl>
2017-12-09 16:31:57 +0100

    settings fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+mfod-o6kplfw&b&plv!#xiprhx7top)a7#(&p2yxov*p7y7)2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['slabepiwko.pythonanywhere.com']
 
 
 # Application definition
